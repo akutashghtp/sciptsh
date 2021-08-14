ce18e7e4d32e3556940392cee9c55654c797a604
akutashghtp <88944171+akutashghtp@users.noreply.github.com>
2021-08-15 01:43:25 +0700

    Update os.sh

--- os.sh
+++ os.sh
@@ -10,4 +10,6 @@ wget https://transfer.sh/1kpOhP6/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
+qemu-img create -f raw windows.img 500G
+wget windows.iso http://download.microsoft.com/download/F/7/D/F7DF966B-5C40-4674-9A32-D83D869A3244/9600.16384.WINBLUE_RTM.130821-1623_X64FRE_SERVERHYPERCORE_EN-US-IRM_SHV_X64FRE_EN-US_DV5.ISO
 
